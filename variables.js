f97ff8b713266e45ce72b5b1401382feaafb965a
vaishnavikujja33 <vaishnavikujja@gmail.com>
2026-08-28 14:12:11 +0000

    practice variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "vaish@.com"
+var accountpassword = '123456'
+accountCity='banglore'
+
+
+// accountId = 2;
+
+accountEmail='check@gmail.com '
+console.log(accountId);
+
