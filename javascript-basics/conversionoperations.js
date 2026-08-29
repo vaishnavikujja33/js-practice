1e78bb50685ad7a77d832c41b6e59ab5d47acfef
vaishnavikujja33 <vaishnavikujja@gmail.com>
2026-08-29 08:47:26 +0000

    conversion of datatypes

---
+++ javascript-basics/conversionoperations.js
@@ -0,0 +1,24 @@
+// let score = 'vaish';
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber )
+// console.log(valueInNumber)
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log (booleanIsLoggedIn);
+
+
+// let name =''
+// let stringname = String(name)
+// console.log(stringname)
+
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
+
